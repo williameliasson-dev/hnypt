dffe96738c4f696ab251aeff7197dc90bcaaae62
williameliasson-dev <williameliasson5@gmail.com>
2024-11-08 19:28:06 +0100

    spec pod

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use k8s_openapi::api::core::v1::Node;
+use kube::{Api, Client};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let client = Client::try_default().await?;
+    let nodes: Api<Node> = Api::all(client);
+    let node_count = nodes.list(&Default::default()).await?.items.len();
+
+    println!("Number of nodes: {}", { node_count });
+
+    Ok(())
 }
