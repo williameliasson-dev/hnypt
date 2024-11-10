c9c71edb7f90e2112ab1092b80c1eb1220a84d57
williameliasson-dev <williameliasson5@gmail.com>
2024-11-10 16:29:52 +0100

    add mongodb compose file

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,10 @@ use pods_management::{get_pod_from_spec, setup_pod};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let honey_pot_pod = get_pod_from_spec("src/pods/honeypot.json").unwrap();
-
     let client = Client::try_default().await?;
     let pods_api: Api<Pod> = Api::default_namespaced(client);
+
+    let honey_pot_pod: Pod = get_pod_from_spec("src/pods/honeypot.json").unwrap();
     setup_pod(honey_pot_pod, &pods_api).await?;
 
     Ok(())
