c76e0e418211186eb4ebb104284fd43fabde5f08
williameliasson-dev <williameliasson5@gmail.com>
2024-11-17 20:49:39 +0100

    setup dotenv

--- src/db.rs
+++ src/db.rs
@@ -1,1 +1,23 @@
+use dotenv::dotenv;
+use mongodb::Client;
+use once_cell::sync::OnceCell;
+use std::env;
 
+static CLIENT: OnceCell<Client> = OnceCell::new();
+
+pub struct MongoDB;
+
+impl MongoDB {
+    pub async fn init() -> mongodb::error::Result<()> {
+        dotenv().ok();
+
+        let uri = env::var("MONGODB_URI").expect("MONGODB_URI not set");
+        let client = Client::with_uri_str(&uri).await?;
+        CLIENT.set(client).unwrap();
+        Ok(())
+    }
+
+    pub fn get_client() -> &'static Client {
+        CLIENT.get().expect("MongoDB client not initialized")
+    }
+}
