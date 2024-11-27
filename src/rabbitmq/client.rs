3be62658c84c45ccfccff79aad98bc21d0bddc8a
williameliasson-dev <williameliasson5@gmail.com>
2024-11-27 23:43:58 +0100

    setup consumer to handle Fn

--- src/rabbitmq/client.rs
+++ src/rabbitmq/client.rs
@@ -1,12 +1,12 @@
 use dotenv::dotenv;
 use futures_lite::stream::StreamExt;
 use lapin::{
-    options::{BasicAckOptions, BasicConsumeOptions, BasicPublishOptions},
+    options::{BasicAckOptions, BasicConsumeOptions, BasicPublishOptions, QueueDeclareOptions},
     types::FieldTable,
     BasicProperties, Channel, Connection, ConnectionProperties,
 };
 use once_cell::sync::OnceCell;
-use std::env;
+use std::{env, future::Future};
 pub struct RabbitMqClient;
 
 static CHANNEL: OnceCell<Channel> = OnceCell::new();
@@ -17,12 +17,20 @@ impl RabbitMqClient {
 
         let uri = match env::var("RABBITMQ_URI") {
             Ok(uri) => uri,
-            Err(_) => panic!("MONGODB_URI not set"),
+            Err(_) => panic!("RABBITMQ_URI not set"),
         };
 
         let conn = Connection::connect(uri.as_str(), ConnectionProperties::default()).await?;
 
         let channel = conn.create_channel().await?;
+
+        channel
+            .queue_declare(
+                "test",
+                QueueDeclareOptions::default(),
+                FieldTable::default(),
+            )
+            .await?;
 
         CHANNEL.set(channel).expect("");
         Ok(())
@@ -34,7 +42,11 @@ impl RabbitMqClient {
             .ok_or_else(|| anyhow::anyhow!("Channel not initialized"))
     }
 
-    pub async fn consume(&self, queue: &str) -> anyhow::Result<()> {
+    pub async fn consume_async<F, Fut>(queue: &str, handler: F) -> anyhow::Result<()>
+    where
+        F: Fn(Vec<u8>) -> Fut + Send + 'static,
+        Fut: Future<Output = anyhow::Result<()>> + Send,
+    {
         let channel = Self::get_channel()?;
 
         let mut consumer = channel
@@ -48,13 +60,14 @@ impl RabbitMqClient {
 
         while let Some(delivery) = consumer.next().await {
             let delivery = delivery.expect("er");
+            handler(delivery.data.clone()).await?;
             delivery.ack(BasicAckOptions::default()).await.expect("ack");
         }
 
         Ok(())
     }
 
-    pub async fn publish(&self, queue: &str, payload: &[u8]) -> anyhow::Result<()> {
+    pub async fn publish(queue: &str, payload: &[u8]) -> anyhow::Result<()> {
         let channel = Self::get_channel()?;
 
         channel
